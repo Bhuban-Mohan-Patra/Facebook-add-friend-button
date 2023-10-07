304436fc366cd8d5c342d9bd1b7cf3273e67878d
Bhuban Mohan Patra <patrabhubanmohan@gmail.com>
2023-10-07 20:36:35 +0530

    remove btn

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const addBtn = document.querySelector("#add")
+const removeBtn = document.querySelector("#remove")
 const rel = document.querySelector("h2")
 
 addBtn.addEventListener("click",(e)=>{
     // const msg = document.createTextNode("Friend")
     rel.innerHTML = "Friend"
     rel.style.color = "green"
+})
+
+\removeBtn.addEventListener("click",(e)=>{
+    // const msg = document.createTextNode("Friend")
+    rel.innerHTML = "Someone Else"
+    rel.style.color = "red"
 })
