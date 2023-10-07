68a3d42bfc35cb47106aadbdd1317ff434a2b9a2
Bhuban Mohan Patra <patrabhubanmohan@gmail.com>
2023-10-07 21:21:00 +0530

    Add and remove in one button

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const addBtn = document.querySelector("#add")
-const removeBtn = document.querySelector("#remove")
+// const removeBtn = document.querySelector("#remove")
 const rel = document.querySelector("h2")
 
+let flag = 0;
 addBtn.addEventListener("click",(e)=>{
-    // const msg = document.createTextNode("Friend")
-    rel.innerHTML = "Friend"
-    rel.style.color = "green"
+    if(flag === 0) {
+        rel.innerHTML = "Friend"
+        rel.style.color = "green"
+        addBtn.innerHTML = "Remove"
+        addBtn.setAttribute("id","remove")
+        flag = 1
+    }
+    else{
+        rel.innerHTML = "Someone Else"
+        rel.style.color = "red"
+        addBtn.innerHTML = "Add Friend"
+        addBtn.removeAttribute("id","remove")
+        flag = 0
+    }
 })
 
-removeBtn.addEventListener("click",(e)=>{
-    // const msg = document.createTextNode("Friend")
-    rel.innerHTML = "Someone Else"
-    rel.style.color = "red"
-})
